0d6531858e6444dd14cffba785c5cdc50e2c1980
Hussaina <zufrah2@yahoo.com>
2022-04-11 09:16:51 +0100

    Second commit

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,4 @@
+from matplotlib import image
 from lms import db,login_manager,app
 from datetime import datetime
 from flask_login import UserMixin
@@ -20,3 +21,13 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f"User('{self.id}','{self.name}','{self.role}','{self.email}')"
+
+
+class Courses(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=False, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    image = db.Column(db.String(120), unique=False, nullable=True)
+    def __repr__(self):
+        return f"Courses('{self.id}','{self.title}','{self.description}','{self.image}')"
--- lms/routes.py
+++ lms/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request ,send_from_directory,make_response,jsonify
 from lms import app,db,bcrypt
-from .forms import Registration,Login,Registerinstructor
+from .forms import Logininstructor, Registration,Login,Registerinstructor
 from .models import User
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.utils import secure_filename
@@ -18,9 +18,22 @@ def dashboard():
     else:
         return redirect(url_for('login'))
 
-@app.route('/logininstructor')
-def logininstructor():
-    return render_template('logininstructor.html')
+@app.route('/admindashboard')
+def admindashboard():
+    if current_user.is_authenticated:
+        return render_template('admindashboard.html')
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/instructordashboard')
+@login_required
+def instructordashboard():
+    if current_user.is_authenticated:
+        return render_template('instructordashboard.html')
+    else:
+        return redirect(url_for('instructorlogin'))
+
 
 
 @app.route('/register', methods=['GET' ,'POST'])
@@ -69,6 +82,27 @@ def login():
     return render_template('login.html',form = form)
 
 
+@app.route('/logininstructor')
+def logininstructor():
+    form = Logininstructor()
+    if current_user.is_authenticated:
+        return redirect(url_for('instructordashboard'))
+    if request.method == "POST":
+        form = request.form
+        email = request.form['email']
+        password = request.form['password']
+        if request.form['rememberme']:
+            remember = request.form['rememberme']
+        else:
+            remember = 'no'
+            user = User.query.filter_by(email=email).first()
+            if user and bcrypt.check_password_hash(user.password, password):
+                login_user(user, remember=remember)
+                if current_user.role== "user":
+                    return redirect(url_for(instructordashboard))
+            else:
+                flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('logininstructor.html',form = form)
 
 
 #@app.route('/registerinstructor')
@@ -99,10 +133,17 @@ def registerinstructor():
             user = User(name=username.lower(),email=email.lower(),password=hashed_password)
             db.session.add(user)
             db.session.commit()
-            return redirect(url_for('registerinstructor'))
+            return redirect(url_for('instructordashboard'))
     return render_template('registerinstructor.html',form = Registerinstructor())# alternatively you can use form = register
     
-@app.route("/logout")
+#@app.route("/logout")
+#def logout():
+  #  logout_user()
+  #  return redirect(url_for('login'))
+
+@app.route("/logout", methods=['GET', 'POST'])
+@login_required
 def logout():
     logout_user()
+    flash("You Have Logged Out Successfully! Remember to keep track of your progress")
     return redirect(url_for('login'))
